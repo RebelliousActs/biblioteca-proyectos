ea6a39a260a4dd2d5d272d273cdbfa58e8b8031d
RebelliousActs <andreactovillas@gmail.com>
2025-12-14 18:49:10 -0600

    feat(auth): implementar inicio de sesión con cuenta de Google usando Socialite

--- config/services.php
+++ config/services.php
@@ -12,7 +12,7 @@ return [
     | location for this type of information, allowing packages to have
     | a conventional file to locate the various service credentials.
     |
-    */
+    
 
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
@@ -32,7 +32,11 @@ return [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
+        ],*/
+        'google' => [
+            'client_id' => env('GOOGLE_CLIENT_ID'),
+            'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+            'redirect' => 'http://biblioteca-proyectos.com/google-auth/callback',
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;   
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,23 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Socialite Routes
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->user();
+    $user = User::updateOrCreate(
+        [
+            'google_id' => $user_google->id,
+        ],
+        [
+            'name' => $user_google->name,
+            'email' => $user_google->email,
+        ]
+    );
+    Auth::login($user);
+    return redirect('/dashboard');
+    
+});
